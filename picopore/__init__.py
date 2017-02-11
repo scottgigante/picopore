0c344664ae87de94247e15645a174294079d521b
Scott Gigante <scottgigante@gmail.com>
2017-02-12 00:45:14 +1100

    fixed h5repack check

--- picopore/__init__.py
+++ picopore/__init__.py
@@ -1,6 +1,6 @@
-from subprocess import call
+from subprocess import call, PIPE
 from util import log
 
-if not call("type h5repack", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0:
-	log("h5repack not installed. Aborting.")
+if not call("type h5repack", shell=True, stdout=PIPE, stderr=PIPE) == 0:
+	log("h5repack (hdf5-tools) not installed. Aborting.")
 	exit(1)
