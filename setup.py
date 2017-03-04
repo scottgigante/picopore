c717ab37c22935de590a67903b1012e9f812b907
Scott Gigante <scottgigante@gmail.com>
2017-03-04 21:41:07 +1100

    fix python version requirement

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ version_py = os.path.join(os.path.dirname(__file__), 'picopore', 'version.py')
 version = open(version_py).read().strip().split('=')[-1].replace('"','').strip()
 print version
 
+if sys.version_info[:2] < (2,7) or (3,0) <= sys.version_info[:2] < (3,4):
+	raise RuntimeError("Python version 2.7 or >=3.4 required.")
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
@@ -18,7 +21,6 @@ setup(
   package_dir={'picopore': "picopore"},
   version=version,
   install_requires=['h5py>2.2.0','watchdog'],
-  requires=['python (>=2.7, !=3.0, !=3.1, !=3.2, !=3.3)'],
   description = 'A tool for reducing the size of Oxford Nanopore Technologies\' datasets without losing information.',
   long_description=read('README.rst'),
   author = 'Scott Gigante',
